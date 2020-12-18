69d76074972b07c894dd8d76286e0b89bf19c875
lewejuice <hamiltonlewis@hotmail.com>
2020-12-18 15:28:36 +0000

    deploying to heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['e-com-app-lewis.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -116,16 +116,18 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-#DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
-#}
 
-DATABASES = {
-    'default': dj_database_url.parse('postgres://xjewiypvzqjrem:184a204d66f797e3fdce943f6c4c4f1a5e4dab5985c483a4e0fc100374f1e3e1@ec2-54-236-146-234.compute-1.amazonaws.com:5432/dfg329if64jjmk')
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
